4dbaf3e2359a6f441cadd8ad5fd17515c2c43395
zhanghx0905 <731931282@qq.com>
2023-02-17 16:43:04 +0800

    similar pages impled

--- index.py
+++ index.py
@@ -16,18 +16,18 @@ class Index:
         titles = [get_tokens(page.title) for page in pages]
 
         self.dictionary = Dictionary(bodies + titles)
-        bodies_bow = [self.dictionary.doc2bow(body) for body in bodies]
-        titles_bow = [self.dictionary.doc2bow(title) for title in titles]
-        corpus = bodies_bow + titles_bow
+        self.bodies_bow = [self.dictionary.doc2bow(body) for body in bodies]
+        self.titles_bow = [self.dictionary.doc2bow(title) for title in titles]
+        corpus = self.bodies_bow + self.titles_bow
         self.tfidf = TfidfModel(corpus, self.dictionary)
         self.bindex = SparseMatrixSimilarity(
-            self.tfidf[bodies_bow], len(self.dictionary)
+            self.tfidf[self.bodies_bow], len(self.dictionary)
         )
         self.tindex = SparseMatrixSimilarity(
-            self.tfidf[titles_bow], len(self.dictionary)
+            self.tfidf[self.titles_bow], len(self.dictionary)
         )
 
-    def search(self, query: str, topk: int = 10) -> list[Page]:
+    def search(self, query: str, topk: int = 10): #-> list[Page]:
         tokens = get_tokens(query)
         query_bow = self.dictionary.doc2bow(tokens)
         query_tfidf = self.tfidf[query_bow]
@@ -37,6 +37,15 @@ class Index:
         indices = np.argpartition(sim, -topk)[-topk:]
         # TODO return page object
         return {sim[id]: self.pages[id].url for id in indices}
+
+    def similar_pages(self, qid: int, topk: int = 5):
+        bsim = self.bindex[self.bodies_bow[qid]]
+        tsim = self.tindex[self.titles_bow[qid]]
+        sim = tsim * 10 + bsim
+        indices = np.argpartition(sim, -topk)[-topk-1:-1]
+        # TODO return page object
+        return {sim[id]: self.pages[id].url for id in indices}
+
 
 
 def load_index():
@@ -51,6 +60,7 @@ def load_index():
 
 if __name__ == "__main__":
     index = load_index()
-    import pprint
+    from pprint import pprint
 
-    pprint.pprint(index.search("hong kong"))
+    pprint(index.search("hong kong"))
+    pprint(index.similar_pages(316))    # PG page
